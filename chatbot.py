e8f65898603d41904aa32a171a9ae347d596fd51
James de Winton <james.dewinton@gmail.com>
2019-04-03 14:12:15 +0100

    fully functional

--- chatbot.py
+++ chatbot.py
@@ -49,3 +49,23 @@ def response(user_response):
     else:
         robo_response = robo_response+sent_tokens[idx]
         return robo_response
+
+flag=True
+print("ROBO: My name is Robo. I will answer your queries about Chatbots. If you want to exit, type Bye!")
+while(flag==True):
+    user_response = input()
+    user_response=user_response.lower()
+    if(user_response!='bye'):
+        if(user_response=='thanks' or user_response=='thank you' ):
+            flag=False
+            print("ROBO: You are welcome..")
+        else:
+            if(greeting(user_response)!=None):
+                print("ROBO: "+greeting(user_response))
+            else:
+                print("ROBO: ",end="")
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+    else:
+        flag=False
+        print("ROBO: Bye! take care..")
