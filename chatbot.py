c7bf4f6fe411955d072a3783a2ada2cf99e76612
James de Winton <james.dewinton@gmail.com>
2019-04-03 13:59:51 +0100

    response function

--- chatbot.py
+++ chatbot.py
@@ -30,3 +30,22 @@ def greeting(sentence):
             return random.choice(GREETING_RESPONSES)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def response(user_response):
+    robo_response=''
+    sent_tokens.append(user_response)
+
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf==0):
+        robo_response=robo_response+"I am sorry! I don't understand you"
+        return robo_response
+    else:
+        robo_response = robo_response+sent_tokens[idx]
+        return robo_response
