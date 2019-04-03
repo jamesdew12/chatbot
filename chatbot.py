192015e460b6e33b710740b00cff4d0e808c3ca2
James de Winton <james.dewinton@gmail.com>
2019-04-03 12:32:11 +0100

    more pre processing

--- chatbot.py
+++ chatbot.py
@@ -10,3 +10,13 @@ nltk.download('punkt') # first-time use only
 nltk.download('wordnet') # first-time use only
 sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences
 word_tokens = nltk.word_tokenize(raw)# converts to list of words
+
+
+
+lemmer = nltk.stem.WordNetLemmatizer()
+#WordNet is a semantically-oriented dictionary of English included in NLTK.
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
