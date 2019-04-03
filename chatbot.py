b22551870d1a7ee984abfc744eaf28c09db228f5
James de Winton <james.dewinton@gmail.com>
2019-04-03 12:16:13 +0100

    coverting sentences to list of words, (pre-processing)

--- chatbot.py
+++ chatbot.py
@@ -2,3 +2,11 @@ import nltk
 import numpy as np
 import random
 import string
+
+f=open('chatbot.txt','r',errors = 'ignore')
+raw=f.read()
+raw=raw.lower()# converts to lowercase
+nltk.download('punkt') # first-time use only
+nltk.download('wordnet') # first-time use only
+sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences
+word_tokens = nltk.word_tokenize(raw)# converts to list of words
